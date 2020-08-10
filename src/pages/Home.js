b63a75457dcab1ffd0abb7e57c08af7e3b0a71fe
Beicker Zambrano <beicker998@gmail.com>
2020-08-10 17:50:28 -0500

    Add home page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Container} from "@material-ui/core"
+
+const Home = () =>{
+  return(
+    <Container>
+      <h1>Hello World</h1>
+    </Container> 
+  )
+
+}
+
+export default Home
